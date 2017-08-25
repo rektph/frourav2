b3f9be7d348c4d24e6c6a5fc1137c6ee39d3582f
rektph <kendrickjaviercosca@gmail.com>
2017-08-26 02:07:59 +0800

    Updated README.md

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ### v2.0
  - Created new ***LoginPage*** (No Connection to the Web)
-  ***ERROR: No 'Access-Control-Allow-Origin' header is present on the requested resource. ***
+  ***ERROR: No 'Access-Control-Allow-Origin' header is present on the requested resource.***
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  user:String;
+  pass:String;
+  userdetails : {
+    id:String,
+    name:String,
+    user:String,
+    pass:String,
+    auth:String
+  }
+  url;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private http:Http) {
+    this.url = "http://bamautomotive.000webhostapp.com/users.php?";
   }
 
+  sendDetails() {
+    this.http.get(this.url+"user=ken"+"&pass=123").map(response => response.json())
+            .subscribe(result => this.userdetails = result.json());
+    console.log(this.userdetails);
+  }
 }
